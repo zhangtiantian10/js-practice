870aa8aff55884830ad8d03a876d960eddaf48d7
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 17:51:51 +0800

    write letter interval
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  const min = Math.min(number_a, number_b);
+  const max = Math.max(number_a, number_b);
+  const result = [];
+
+  for (let i = min; i <= max; i++) {
+
+    result.push(String.fromCharCode(96 + i));
+  }
+
+  return number_a > number_b ? result.reverse() : result;
 }
 
 module.exports = get_letter_interval;
