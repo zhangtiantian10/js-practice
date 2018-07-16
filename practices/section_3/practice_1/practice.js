4c7728dbf1be3980437e2bcb609ae9e34c1daf76
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:17:03 +0800

    write section 3 create update collection practice 1
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,5 +1,11 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  return collection_a.map(a => {
+    if (object_b.value.includes(a.key)) {
+      a.count--;
+    }
+
+    return a;
+  })
 }
 
 module.exports = create_updated_collection;
