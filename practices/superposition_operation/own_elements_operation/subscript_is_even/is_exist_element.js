4e5f803a6bc28d18494d8c91b7ea109d704892bb
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:52:04 +0800

    write is exit element
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,6 @@
 'use strict';
 var is_exist_element = function(collection,element){
 
+  return collection.find((c, index) => index % 2 === 0 && c === element) !== undefined;
 };
 module.exports = is_exist_element;
