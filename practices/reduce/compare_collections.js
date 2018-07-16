02292509892237b14c80770a2db0de2c30e2c39d
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:29:51 +0800

    write compare collections
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+
+  return collection_a.join(',') === collection_b.join(',');
 }
 
 module.exports = compare_collections;
