ee9e56391a4945284e52d8fd58d48ad3a9b836b1
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:28:22 +0800

    write letter mapping 2
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -2,7 +2,8 @@
 
 function average_to_letter(collection) {
 
-  //在这里写入代码
+  const code = Math.round(collection.reduce((a, b) => a + b) / collection.length);
+  return String.fromCharCode(96 + code);
 }
 
 module.exports = average_to_letter;
