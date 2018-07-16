1eae676a1efbf3d89726ab73c6a667abca3e1542
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:35:17 +0800

    write average uneven
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function average_uneven(collection) {
+  const unevens = collection.filter(c => c % 2 !== 0);
 
-  //在这里写入代码
+  return unevens.reduce((a, b) => a + b) / unevens.length;
 }
 
 module.exports = average_uneven;
