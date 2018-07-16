ebb2a72f3e64d7af66a3a76ebd05977e41a4555c
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 21:00:38 +0800

    write letter interval 2
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,30 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  const min = Math.min(number_a, number_b);
+  const max = Math.max(number_a, number_b);
+  const result = [];
+
+  for (let i = min; i <= max; i++) {
+    let divisor = parseInt(i / 26, 10);
+    let residue = i % 26;
+    
+    if (residue === 0) {
+      divisor --;
+      residue = 26
+    }
+    
+    const singleDigit = String.fromCharCode(96 + residue);
+    let tenDigits = "";
+
+    if (divisor) {
+        tenDigits = String.fromCharCode(96 + divisor);
+    }
+
+    result.push(tenDigits.concat(singleDigit));
+  }
+
+  return number_a > number_b ? result.reverse() : result;
 }
 
 module.exports = get_letter_interval_2;
