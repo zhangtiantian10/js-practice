7e179bf42910faed5f16ab5b7d9c9351ee57ef2f
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:44:37 +0800

    write compute chain median
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  const array = collection.split('->').map(c => parseInt(c, 10)).sort((a, b) => a - b);
+
+  let medianIndex = parseInt(array.length / 2, 10);
+  let median = array[medianIndex];
+
+  if (array.length % 2 === 0) {
+    median = (median + array[array.length / 2 - 1]) / 2;
+  }
+
+  return median;
+
 }
 
 module.exports = compute_chain_median;
