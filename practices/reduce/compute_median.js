531e061125edf51f2324e6991d4a2addb8756553
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:48:16 +0800

    write compute median
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  collection = collection.sort((a, b) => a - b);
+  let medianIndex = parseInt(collection.length / 2, 10);
+  let median = collection[medianIndex];
+
+  if (collection.length % 2 === 0) {
+    median = (median + collection[collection.length / 2 - 1]) / 2;
+  }
+
+  return median;
+
 }
 
 module.exports = compute_median;
