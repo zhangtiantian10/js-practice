e7da085f20e049676434146fd29bded577298d96
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:55:18 +0800

    write even asc uneven desc
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,9 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
 
+  const evens = collection.filter(c => c % 2 === 0).sort((a, b) => a - b);
+  const unevens = collection.filter(c => c % 2 !== 0).sort((a, b) => b - a);
+
+  return evens.concat(unevens);
 };
 module.exports = even_asc_odd_desc;
