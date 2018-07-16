c939eb74192f98871d2003dc3726fc264900b15c
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 21:53:34 +0800

    write two collections practice 3
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter(a => {
+    const array = collection_b.filter(b => a % b === 0);
+    return array.length;
+  })
 }
 
 module.exports = choose_divisible_integer;
