2b8738ea927c6296e0bf20df15749cd737926218
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:56:45 +0800

    write section 1 collect same elements practice 2
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  collection_b = [].concat.apply([], collection_b);
+
+  return collection_a.filter(a => collection_b.includes(a));
 }
 
 module.exports = collect_same_elements;
