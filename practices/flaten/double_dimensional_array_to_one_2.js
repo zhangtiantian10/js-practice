b8488a3e07c05ae9bc78e038d952d77c40ed6f2c
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:08:17 +0800

    write double dimensional array to one 2
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  const newCollection = [].concat.apply([], collection);
+  const result = [];
+  newCollection.forEach(n => {
+    if (!result.includes(n)) {
+      result.push(n);
+    }
+  })
+  return result;
 }
 
 module.exports = double_to_one;
