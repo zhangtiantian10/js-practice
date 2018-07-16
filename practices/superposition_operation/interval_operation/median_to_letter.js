953b213476c52c79983b883e7b3e2d02aee2f653
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:41:11 +0800

    write median to letter
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -2,7 +2,35 @@
 
 function median_to_letter(collection) {
 
-  //在这里写入代码
+  collection = collection.sort((a, b) => a - b);
+
+  let medianIndex = parseInt(collection.length / 2, 10);
+  let median = collection[medianIndex];
+
+  if (collection.length % 2 === 0) {
+    median = Math.round((median + collection[collection.length / 2 - 1]) / 2);
+  }
+
+  return changeCodeToString(median);
+}
+
+function changeCodeToString(number) {
+  let divisor = parseInt(number / 26, 10);
+  let residue = number % 26;
+
+  if (residue === 0) {
+    divisor --;
+    residue = 26
+  }
+
+  const singleDigit = String.fromCharCode(96 + residue);
+  let tenDigits = "";
+
+  if (divisor) {
+    tenDigits = String.fromCharCode(96 + divisor);
+  }
+
+  return tenDigits + singleDigit;
 }
 
 module.exports = median_to_letter;
