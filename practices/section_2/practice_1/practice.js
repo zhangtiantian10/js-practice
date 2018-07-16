e4ed6929d266165346462fdd63676e26d683e237
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:10:52 +0800

    write section 2 collect same elements practice 1
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,24 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  const results = [];
+
+  for (let c of collection) {
+    countNumber(c, results);
+  }
+
+  return results;
+}
+
+function countNumber(c, results) {
+  let result = results.find(re => re.key === c);
+
+  if (result) {
+    result.count++;
+  } else {
+    result = {key: c, count: 1};
+    results.push(result);
+  }
+
+  return results;
 }
 
 module.exports = count_same_elements;
