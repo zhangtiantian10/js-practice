9d49a1753c4b292502c21ce88cddcbc0910d155b
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 00:23:44 +0800

    write single element
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,6 @@
 'use strict';
 var single_element = function(collection){
-
+  const array = collection.filter((c, index) => (index + 1) % 2 === 0);
+  return array.filter((a, index) => index === array.lastIndexOf(a) && index === array.indexOf(a)).sort((a, b) => a - b);
 };
 module.exports = single_element;
