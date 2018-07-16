d0d2c4b6db5582e5d1310b6b190cb432facab063
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:16:07 +0800

    write number map to word over 26
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,24 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+
+  return collection.map(c => {
+    let divisor = parseInt(c / 26, 10);
+    let residue = c % 26;
+
+    if (residue === 0) {
+      divisor --;
+      residue = 26
+    }
+
+    const singleDigit = String.fromCharCode(96 + residue);
+    let tenDigits = "";
+
+    if (divisor) {
+      tenDigits = String.fromCharCode(96 + divisor);
+    }
+
+    return tenDigits + singleDigit;
+  });
 };
 
 module.exports = number_map_to_word_over_26;
