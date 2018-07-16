0a1461e8114076f8259d8e103df703ffde544ed5
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 14:10:46 +0800

    write grouping count
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,24 @@
 'use strict';
 
 function grouping_count(collection) {
+  let output = {};
 
-  //在这里写入代码
+  collection.forEach(c => {
+    output = countNumber(c, output);
+  })
+
+  return output;
+}
+
+function countNumber(str, object) {
+  let key = Object.keys(object).find(o => parseInt(o) === str);
+  if (key) {
+    object[key] ++;
+  } else {
+    object[str] = 1;
+  }
+
+  return object;
 }
 
 module.exports = grouping_count;
