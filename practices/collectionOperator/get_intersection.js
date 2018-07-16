61bc0996cb5b605a66bccd223850c598298e2d93
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 17:40:35 +0800

    write intersection
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  return collection_b.filter(b => collection_a.includes(b))
 }
 
 module.exports = get_intersection;
