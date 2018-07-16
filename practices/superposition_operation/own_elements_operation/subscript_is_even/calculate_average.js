d03ca7bbf898e814b116e424ca50f439a5b3868f
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:43:36 +0800

    write calculate average
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,8 @@
 'use strict';
 var calculate_average = function(collection){
 
+  const evens = collection.filter(c => c % 2 === 0);
+
+  return evens.reduce((a, b) => a + b) / evens.length;
 };
 module.exports = calculate_average;
