3719a0510a6449b478756282fd307a6e3dd9f95b
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:59:25 +0800

    write one add next multipley three
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,15 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+  const array = collection.map((c, index) => {
+    if (index + 1 !== collection.length) {
+      return (c + collection[index + 1]) * 3;
+    } else {
+      return '';
+    }
+  });
+
+  array.splice(array.length - 1, 1);
+
+  return array;
 }
 module.exports = one_add_next_multiply_three;
