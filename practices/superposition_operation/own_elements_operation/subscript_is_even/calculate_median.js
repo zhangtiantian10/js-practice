7266eb8ccce824c9469ac81d571b23f1732b2b0d
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 23:46:23 +0800

    write calculate median
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,14 @@
 'use strict';
 var calculate_median = function(collection){
 
+  const evens = collection.filter(c => c % 2 === 0).sort((a, b) => a - b);
+  let medianIndex = parseInt(evens.length / 2, 10);
+  let median = evens[medianIndex];
+
+  if (evens.length % 2 === 0) {
+    median = (median + evens[evens.length / 2 - 1]) / 2;
+  }
+
+  return median;
 };
 module.exports = calculate_median;
