3189fb6c33117d95ce3e6dbeffcbf719e0d617cd
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 17:36:49 +0800

    write get integer interval 2
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  const min = Math.min(number_a, number_b);
+  const max = Math.max(number_a, number_b);
+  const result = [];
+
+  for (let i = min; i <= max; i++) {
+    if (i % 2 === 0) {
+      result.push(i);
+    }
+  }
+
+  return number_a > number_b ? result.reverse() : result;
 }
 
 module.exports = get_integer_interval_2;
