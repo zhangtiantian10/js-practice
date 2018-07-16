0ca8f2c63a4887536b60ff997900eb9f2917f6c5
Tian Zhang <tizhang@CNtizhang.local>
2018-07-17 00:08:37 +0800

    write rank by two large one small
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,17 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  collection = collection.sort((a, b) => a - b);
+
+  return collection.map((c, index) => {
+    if (index % 3 === 0 && index + 2 < collection.length) {
+      return collection[index + 1];
+    } else if (index % 3 === 2) {
+      return collection[index - 2];
+    } else if (index + 2 < collection.length) {
+      return collection[index + 1];
+    } else {
+      return c;
+    }
+  })
 }
 module.exports = rank_by_two_large_one_small;
