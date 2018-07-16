88847be067dd2a404b04bf1d0c3af59d23f27c03
Tian Zhang <tizhang@CNtizhang.local>
2018-07-16 22:38:56 +0800

    fix bug
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,6 @@
 'use strict';
 var rank_asc = function(collection){
-  return collection.sort((a, b) => a < b);
+  return collection.sort((a, b) => b - a);
 };
 
 module.exports = rank_asc;
--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,6 @@
 'use strict';
 var rank_desc = function(collection){
-  return collection.sort();
+  return collection.sort((a, b) => a - b);
 };
 
 module.exports = rank_desc;
